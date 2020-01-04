d299e84e4d432b913218bd77d54e03da65c46e68
qinyuyao <3255458795@qq.com>
2020-01-04 14:12:37 +0800

    Initial Commit

---
+++ FileInfo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FileInfo
+//
+//  Created by qinyuyao on 2020/1/4.
+//  Copyright © 2020 qinyuyao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FileInfo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileInfo
+//
+//  Created by qinyuyao on 2020/1/4.
+//  Copyright © 2020 qinyuyao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
